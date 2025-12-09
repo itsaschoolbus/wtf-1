d625eb22d8bcdc70c6f5e44ba1986933194ab722
mk7x7x <mikxelsilvax777@gmail.com>
2025-12-09 08:08:38 +0000

    kernel: switch to aosp clang and add some flags

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# Add Clang For Compile Kernel
+cd prebuilts/clang/host/linux-x86
+mkdir clang-r584948 ; cd clang-r584948 ; wget https://github.com/MrShockWAVEog/Google-Clang-Mirror/releases/download/clang-r584948-aosp/clang.tar.gz ; tar -xf * && ls
+cd ../../../../..
