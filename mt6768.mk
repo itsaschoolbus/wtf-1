199ad417827ca9b1d4d5607700f80eb37e33b41d
mk7x7x <mikxelsilvax777@gmail.com>
2025-12-22 10:22:10 +0000

    prebuiilts: add via Browser

--- mt6768.mk
+++ mt6768.mk
@@ -37,6 +37,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MtkInCallService
 
+# Prebuilts
+PRODUCT_PACKAGES += \
+    Via
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
